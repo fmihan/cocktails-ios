9368479d77ff3d79f85aa36ed1c90d8af4fc4b40
Fabijan Mihanović <mihanovic.fabijan@gmail.com>
2024-05-23 19:41:53 +0200

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Fabijan Mihanović on 23.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
