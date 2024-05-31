8b25d37bf2d99c65596d5593a406851a3430707c
Fabijan Mihanović <mihanovic.fabijan@gmail.com>
2024-05-31 18:39:38 +0200

    Minor refactorint

--- Cocktails/Extensions/UIKit/UINavigationControllerExtension.swift
+++ Cocktails/Extensions/UIKit/UINavigationControllerExtension.swift
@@ -9,9 +9,9 @@ import UIKit
 
 extension UINavigationController {
 
-  open override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
-    navigationBar.topItem?.backButtonDisplayMode = .minimal
-  }
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        navigationBar.topItem?.backButtonDisplayMode = .minimal
+    }
 
 }
